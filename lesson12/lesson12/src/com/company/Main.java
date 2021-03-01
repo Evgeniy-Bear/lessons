fa4c081b9c9021415e2630de8effd7bced63692a
Evgeniy-Bear <Gennadith.bear@gmail.com>
2021-03-01 20:41:25 +0500

    add

--- lesson12/lesson12/src/com/company/Main.java
+++ lesson12/lesson12/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         
+        try {int x=100/0;
+
+        }
+        catch (Aritmetic){
+            System.out.println("ERoorr");
+        }
     }
 
 }
